6aeeee140f94f8d62c7b4c9c08e96f0066848710
juliohusak <julio.husak@bol.com.br>
2022-10-05 16:51:35 -0300

    AutomacaoAPP

---
+++ features/nav.feature
@@ -0,0 +1,8 @@
+#language: pt
+
+Funcionalidade: Navegação
+
+    Cenario: Login CPF Válido
+
+        Quando clico no botão entrar
+        Então  vejo a campo para digitar o CPF válido
---
+++ features/support/hooks.rb
@@ -0,0 +1,23 @@
+Before do
+  driver.start_driver
+  driver.manage.timeouts.implicit_wait = 15
+  # Evento de click no botão Entrar.
+
+end
+
+After do |scenario|
+  binary_shot = driver.screenshot_as(:base64)
+
+  temp_shot = "logs/temp_shot.png"
+
+  File.open(temp_shot, "wb") do |f|
+    f.write(Base64.decode64(binary_shot).force_encoding("UTF-8"))
+  end
+
+  Allure.add_attachment(
+    name: "screenshot",
+    type: Allure::ContentType::PNG,
+    source: File.open(temp_shot),
+  )
+  driver.quit_driver
+end
